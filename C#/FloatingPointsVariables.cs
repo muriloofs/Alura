bd5bedb6dd00cb8d71eba66633282d8fd6319ebf
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-18 08:41:18 -0300

    Create FloatingPointsVariables.cs

---
+++ C#/FloatingPointsVariables.cs
@@ -0,0 +1,25 @@
+using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario;
+        salario = 8000.10;
+
+        salario = 8000;
+
+        double idade;
+
+        idade = 9.0 / 4;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para finalizar o programa.");
+        Console.ReadLine();
+
+    }
+}
