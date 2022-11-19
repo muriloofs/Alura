0b70bcb5330e2cd9d35801752ca4e9278ab256d3
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-19 10:51:05 -0300

    Create While.cs

---
+++ C#/Getting Started in C#/While.cs
@@ -0,0 +1,13 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int contador = 0;
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador++;
+        }
+        Console.ReadLine();
+    }
+}
