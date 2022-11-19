49376a4590b281131741bbea474a66e2d59ad160
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-19 10:58:30 -0300

    Create For.cs

---
+++ C#/Getting Started in C#/For.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int contador = 0; contador <= 115; contador++)
+        {
+            Console.WriteLine(contador);
+        }
+        Console.ReadKey();
+    }
+}
