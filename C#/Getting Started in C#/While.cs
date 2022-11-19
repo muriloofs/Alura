bad5c6881dcc43debd8b3ea9e35dd416b5975abf
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-19 10:52:21 -0300

    Update While.cs

--- C#/Getting Started in C#/While.cs
+++ C#/Getting Started in C#/While.cs
@@ -3,11 +3,11 @@ public class Program
     static void Main(string[] args)
     {
         int contador = 0;
-        while (contador <= 10)
+        while (contador <= 164)
         {
             Console.WriteLine(contador);
             contador++;
         }
-        Console.ReadLine();
+        Console.ReadKey();
     }
 }
