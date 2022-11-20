f679857aa5349e6ae64fafccb41ccc276b32a538
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-19 23:51:04 -0300

    Create Factorial.cs

---
+++ C#/Getting Started in C#/Factorial.cs
@@ -0,0 +1,13 @@
+using System;
+class Programa
+{
+    static void Main(String[] args)
+    {
+        int fatorial = 1;
+        for (int i = 1; i < 11; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+        }
+    }
+}
