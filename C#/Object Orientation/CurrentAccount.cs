61c4a39ba471d8d4267e70a645840c866cf1348f
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-22 19:48:08 -0300

    Create CurrentAccount.cs

---
+++ C#/Object Orientation/CurrentAccount.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class CurrentAccount
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public double saldo;
+    }
+}
