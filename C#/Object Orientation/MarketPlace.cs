a1e2363cc8beb847e4aa54a58d2719c277719689
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-28 19:52:01 -0300

    Add files via upload

---
+++ C#/Object Orientation/MarketPlace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class MarketPlace
+    {
+        public string nome;
+        public string fabricante;
+        public int codigoDeBarras;
+        public double valorUnitario;
+        public double valorDeCompra;
+        public int quantidadeEmEstoque;
+    }
+}
---
+++ C#/Object Orientation/MedicalSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class MedicalSystem
+    {
+        public string name;
+        public int age;
+        public double height;
+        public double weight;
+    }
+}
